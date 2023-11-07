da3efee36b5d974a786e830a137b5785c5159f8c
FranzHaidnor <haidnor@outlook.com>
2023-11-07 09:04:59 +0800

    update GetLogFolderProcessor

--- log-client/src/main/java/haidnor/log/client/netty/processor/GetLogFolderProcessor.java
+++ log-client/src/main/java/haidnor/log/client/netty/processor/GetLogFolderProcessor.java
@@ -2,22 +2,21 @@ package haidnor.log.client.netty.processor;
 
 import cn.hutool.core.io.FileUtil;
 import haidnor.log.common.model.GetLogFolderRequest;
-import haidnor.log.common.model.GetLogRequest;
 import haidnor.log.common.util.Jackson;
-import haidnor.log.common.util.LogUtil;
 import haidnor.remoting.core.NettyRequestProcessor;
 import haidnor.remoting.protocol.RemotingCommand;
 import haidnor.remoting.protocol.RemotingSysResponseCode;
 import io.netty.channel.ChannelHandlerContext;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class GetLogFolderProcessor implements NettyRequestProcessor {
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Override
     public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand remotingCommand) {
@@ -26,7 +25,12 @@ public class GetLogFolderProcessor implements NettyRequestProcessor {
             Set<String> folderNameSet = new HashSet<>();
             List<File> files = FileUtil.loopFiles(FileUtil.file(request.getPath()), 1, File::isDirectory);
             for (File file : files) {
-                folderNameSet.add(file.getName());
+                try {
+                    simpleDateFormat.parse(file.getName());
+                    folderNameSet.add(file.getName());
+                } catch (Exception exception) {
+                    // nop
+                }
             }
             return RemotingCommand.createResponse(RemotingSysResponseCode.SUCCESS, Jackson.toJsonBytes(folderNameSet));
         } catch (Exception exception) {
