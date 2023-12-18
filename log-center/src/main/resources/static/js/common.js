6ebe939a3cce511cc854b52104983d71bf8bdf09
Franz Haidnor <haidnor@outlook.com>
2023-12-18 11:37:23 +0800

    update

--- log-center/src/main/resources/static/js/common.js
+++
@@ -1,9 +0,0 @@
-console.log("author wang xiang 276644985@qq.com")
-
-function getRootPath() {
-    return "http://127.0.0.1:8080"
-}
-
-function getWSRootPath() {
-    return "ws://127.0.0.1:8080/webSocket"
-}
